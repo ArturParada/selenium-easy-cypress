f9d8d8a53b30017d8e352e9ac4eba380e08082d0
Artur <paradaartur@gmail.com>
2023-05-30 18:32:27 +0200

    checkbox

--- cypress/e2e/checkBoxDemo.js
+++ cypress/e2e/checkBoxDemo.js
@@ -5,9 +5,30 @@ describe('Checkbox demo', () => {
         CheckBoxDemoPO.goCheckboxDemoPage()
     })
 
-    it('checkbox checked', () => {
+    it('After click on single checkbox user should get valid message', () => {
         CheckBoxDemoPO.checkSingleInputAndVeryfi()
         CheckBoxDemoPO.receivedMessageShouldBeEqualExpected("Success - Check box is checked")
+    })
+    it('After click on Check All button , all checkbox should be checked', () => {
+        CheckBoxDemoPO.checkAllBtnClick()
+        CheckBoxDemoPO.receivedMessageOnBtn('Uncheck All')
+
+    })
+    it("After check three option , btn didn't change name", () => {
+        for (let i = 0; i < 3; i++) {
+            CheckBoxDemoPO.selectMultiCheckboxe(i)
+        }
+        CheckBoxDemoPO.receivedMessageOnBtn("Check All")
+
+
+    })
+    it("After check four option , btn change name", () => {
+        for (let i = 0; i < 4; i++) {
+            CheckBoxDemoPO.selectMultiCheckboxe(i)
+        }
+        CheckBoxDemoPO.receivedMessageOnBtn("Uncheck All")
+
+
     })
 
 
--- cypress/support/PageObjects/CheckBoxDemoPO.js
+++ cypress/support/PageObjects/CheckBoxDemoPO.js
@@ -5,6 +5,26 @@ class CheckBoxDemoPO {
     get receivedMessageElement() {
         return cy.get('#txtAge')
     }
+    get checkAllBtnElement() {
+        return cy.get('input#check1')
+    }
+
+    get findAllCheckboxElement() {
+        return cy.get("input.cb1-element")
+    }
+
+    selectMultiCheckboxe(value) {
+
+        this.findAllCheckboxElement.eq(value).click()
+    }
+
+    checkAllBtnClick() {
+        this.checkAllBtnElement.click()
+    }
+
+    receivedMessageOnBtn(text) {
+        this.checkAllBtnElement.should('have.value', text)
+    }
 
     receivedMessageShouldBeEqualExpected(checkboxMessage) {
         this.receivedMessageElement.invoke('text').should('contain', checkboxMessage)
