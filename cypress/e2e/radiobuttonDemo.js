034ec6471a8b9e1c6cee532b1fd707f1d69dc08d
Artur <paradaartur@gmail.com>
2023-06-03 18:47:19 +0200

    radio second

--- cypress/e2e/radiobuttonDemo.js
+++ cypress/e2e/radiobuttonDemo.js
@@ -9,13 +9,31 @@ describe('Radibutton tests', () => {
     ]
     data.forEach($genders => {
         const [gender, message] = $genders
-        it(`Select a gender${gender} and print the result after clicking the button${message}`, () => {
-            RadioButtonPO.selectSinglegender(gender)
+        it(`Select a gender ${gender} and print the result after clicking the button ${message}`, () => {
+            RadioButtonPO.selectSingleGender(gender)
             RadioButtonPO.clickCheckedValueBtn()
             RadioButtonPO.assertValueChecked(message)
 
         });
     });
+    const dataFotGroup =
+        [
+            ['Male', "0 to 5"],
+            ['Male', "5 to 15"],
+            ['Male', "15 to 50"],
+            ['Female', "0 to 5"],
+            ['Female', "5 to 15"],
+            ['Female', "15 to 50"],
+        ]
+    dataFotGroup.forEach($data => {
+        const [gender, ageGroup] = $data
+        it(`Selec a ${gender} , age group ${ageGroup} and print message`, () => {
+            RadioButtonPO.selectGroupGender(gender)
+            RadioButtonPO.selectAgeGroup(ageGroup)
+
+        });
+    })
+
 
 
 });
--- cypress/support/PageObjects/RadioButtonPO.js
+++ cypress/support/PageObjects/RadioButtonPO.js
@@ -1,23 +1,55 @@
 class RadioButtonPo {
-    get radioGendersSelector() {
+    get radioGendersSelectorElement() {
         return cy.get("[name='optradio']")
     }
-    get getChceckedValueBtnElement() {
+
+    get chceckedValueBtnElement() {
         return cy.get("button#buttoncheck")
     }
+
     get checkedValueElement() {
         return cy.get("p.radiobutton")
     }
-    clickCheckedValueBtn() {
-        this.getChceckedValueBtnElement.click()
+
+    get radioGendersSelectorGroupElement() {
+        return cy.get("[name='gender']")
     }
+
+    get ageGroupElement() {
+        return cy.get("[name='ageGroup']")
+    }
+
+    // get getValuesBtnElement
+
+    clickCheckedValueBtn() {
+        this.chceckedValueBtnElement.click()
+    }
+
     assertValueChecked(gender) {
         this.checkedValueElement.should('have.text', gender)
 
     }
 
-    selectSinglegender(gender) {
-        this.radioGendersSelector.invoke("attr", "val").contains(gender).click()
+    selectSingleGender(gender) {
+        this.radioGendersSelectorElement.invoke("attr", "val").contains(gender).click()
+    }
+
+    selectGroupGender(gender) {
+        if (gender === "Male") {
+            this.radioGendersSelectorGroupElement.eq(0).click()
+        }
+        else {
+            this.radioGendersSelectorGroupElement.eq(1).click()
+        }
+    }
+    selectAgeGroup(ageGroup) {
+        if (ageGroup === "0 to 5") {
+            this.ageGroupElement.eq(0).click()
+        } else if (ageGroup === "5 to 15") {
+            this.ageGroupElement.eq(1).click()
+        } else if (ageGroup === "15 to 50") {
+            this.ageGroupElement.eq(1).click()
+        }
     }
 
     goRadiobuttonDemoPage() {
