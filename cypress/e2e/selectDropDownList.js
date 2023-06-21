1998026b4b1eeb1ed9caab4e08538db59dd3423e
Artur <paradaartur@gmail.com>
2023-06-21 19:51:41 +0200

    dropdown

--- cypress/e2e/selectDropDownList.js
+++ cypress/e2e/selectDropDownList.js
@@ -1,5 +1,11 @@
+import dropDownPO from "../support/PageObjects/dropDownPO";
 describe('Drop down test', () => {
+    beforeEach(() => {
+        dropDownPO.goBasicSelectDropdownDemoPage()
+    })
     it('Day selec', () => {
-
+        const day = "Monday"
+        dropDownPO.selectDayFromDropdown(day)
+        dropDownPO.recaivedSingleMessage(day)
     });
 });
--- cypress/support/PageObjects/dropDownPO.js
+++ cypress/support/PageObjects/dropDownPO.js
@@ -1,4 +1,22 @@
 class DropDownPO {
+    get singleDropdownElement() {
+        return cy.get('select#select-demo')
+    }
 
+    get singleMessageElement() {
+        return cy.get('p.selected-value')
+    }
+
+    recaivedSingleMessage(value) {
+        this.singleMessageElement.should("have.text", `Day selected :- ${value}`)
+    }
+
+    selectDayFromDropdown(value) {
+        this.singleDropdownElement.select(value)
+    }
+
+    goBasicSelectDropdownDemoPage() {
+        cy.visit("/basic-select-dropdown-demo.html")
+    }
 }
 export default new DropDownPO()
