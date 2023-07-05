01e3dd7fc02584d002ea70f410157c83cfa01ddc
Artur <paradaartur@gmail.com>
2023-07-05 19:34:48 +0200

    cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
-
+const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber())
       // implement node event listeners here
     },
-    specPattern: "cypress/e2e/**/*.js",
-    baseUrl: "http://demo.seleniumeasy.com/"
+    specPattern: "cypress/e2e/**/*.{js.jsx,ts,tsx,feature}",
+    baseUrl: "http://demo.seleniumeasy.com/",
+    experimentalStudio: true
   },
 });
