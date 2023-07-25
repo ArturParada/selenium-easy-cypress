79a1be7987b59588961503aaf54e5c6639671363
Artur <paradaartur@gmail.com>
2023-07-25 15:14:53 +0200

    jenkins fix

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = defineConfig({
+  projectId: 'bf8kgd',
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
