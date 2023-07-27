5c30642252007526a6e6482b0fcf1d8f09b90bf5
Artur <paradaartur@gmail.com>
2023-07-27 17:28:29 +0200

    Revert "testowy22"
    This reverts commit 6be9828b38f6521aa77ae36f237642cee697ef27.

--- cypress/e2e/jsAlertsDemo.js
+++ cypress/e2e/jsAlertsDemo.js
@@ -5,19 +5,21 @@ describe('Alerts tests suit', () => {
     beforeEach(() => {
         AlertsDemoPO.goAlertHomePge()
     })
-
     it('Correct message after click initial alert button', () => {
         AlertsDemoPO.clickOninitialAlertBoxBTN()
         AlertsDemoPO.validatiOnCorrectAlertBoxMessage()
-    });
 
+    });
     it('Correct message after click "ok" button on alert', () => {
         AlertsDemoPO.clickonInitialConfirmBoxBtN()
         AlertsDemoPO.validationCorrectConfirmBoxMessage("Press a button!", true, "You pressed OK!")
-    });
 
+
+    });
     it('Correct message after click "cancel" button on alert', () => {
         AlertsDemoPO.clickonInitialConfirmBoxBtN()
         AlertsDemoPO.validationCorrectConfirmBoxMessage("Press a button!", false, 'You pressed Cancel!')
+
+
     });
 });
